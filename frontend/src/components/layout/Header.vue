e2c830ebd4152d40c81301a7721d4a97bf15699f
BajicMladen <bajicmladen@uns.ac.rs>
2023-04-29 16:08:49 +0200

    Remove unnecesery components and update folder structure

---
+++ frontend/src/components/layout/Header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import cart from '../..//assets/cart.png'
+import user from '../..//assets/user.png'
+</script>
+
+<template>
+  <div class="flex flex-row items-center justify-between px-24 mb-10 bg-slate-600 h-20">
+    <div class="text-2xl text-white">NBTG</div>
+    <div class="flex flex-row text-white text-lg">
+      <div class="flex flex-row">
+        <img :src="cart" alt="login_icon" class="mr-2 w-5 h-5 mt-1" />
+        <RouterLink to="/cart" class="mr-8">Cart</RouterLink>
+      </div>
+      <div class="flex flex-row">
+        <img :src="user" alt="login_icon" class="mr-2 w-5 h-5 mt-1" />
+        <RouterLink to="/login">Login</RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ frontend/src/components/layout/TradeMark.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center justify-center w-full h-7 text-gray-600 text-md">
+    Copyright © NBTG
+  </div>
+</template>
